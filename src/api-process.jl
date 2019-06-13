bf5b20fa9191be0a5ce04bad55ea85d208752ee0
Xiaoqi Hu <xiaoqihu@uthsc.edu>
2019-06-13 16:33:40 -0500

    adding json to df function

--- src/api-process.jl
+++ src/api-process.jl
@@ -5,8 +5,12 @@
 
 
 using HTTP
-using LazyJSON #using LazyJSON package because JSON package is considerable slower
-
+# using LazyJSON #using LazyJSON package because JSON package is considerable slower
+using DataFrames
+using JSON
+# using DelimitedFiles
+using CSV
+using BenchmarkTools
 # using DataFramesIO
 
 
@@ -16,7 +20,7 @@ function get_api(url)
 end
 
 function parse_json(str)
-    return LazyJSON.parse(str)
+    return JSON.parse(str)
 end
 
 function process_csv_file(input::String; delim=',', comments=false)
@@ -42,4 +46,62 @@ function count_extra_comment_lines(array)
     return iter
 end
 
+function json2mat(s::String)
+    json = JSON.parse(s)
+    nrows = length(json)
+    colnames = sort(collect(keys(json[1])))
+    ncols = length(colnames)
 
+    symbols = Array{Symbol}(undef, ncols)
+    for i in 1:ncols
+        symbols[i] = Symbol(colnames[i])
+    end
+
+    mat = Array{Any}(undef, nrows, ncols)
+    for i in 1:ncols 
+        for j in 1:nrows
+            mat[j,i] = get(json[j], String(colnames[i]), "NA")
+        end
+    end
+
+    res1 = convert_df($mat, $symbols)
+    # res2 = convert_df_slow($mat, $symbols)
+
+    res1
+    
+end
+
+
+
+function convert_df(mat::Array{Any, 2}, symbols::Array{Symbol,1})
+    df = DataFrame()
+    for i in 1:length(symbols)
+        # tying to use meta programming to convert data frame column type from Any to appropriate type such as String or Int
+        # note to self: failed because converting any to a string array vs converting any to Int array is different syntax
+        # c = i
+        # s = symbols[i]
+        # println("$c, $(typeof(s))")
+        # eval(:(typeof(df[1,c]))).(df[s])
+        # convert.(eval(:(typeof(df[1,c]))), df[s])
+
+        # trying to add one column of data at a time. 
+        # This method requires previous knowledge about data.
+        # df[symbols[i]] = mat[:,i]
+        if (typeof(mat[1,i]) == String)
+            df[symbols[i]] = String.(mat[:,i])
+            # println("converting to string : type: $(typeof(df[symbols[i]]))")
+        elseif (typeof(mat[1,i]) == Int64)
+            df[symbols[i]] = convert.(Int64, mat[:,i])
+        end        
+    end
+    return df 
+end
+
+function convert_df_slow(mat::Array{Any, 2}, symbols::Array{Symbol,1})
+    # trying to convert dataframe data type without previous knowledge about data. 
+    df = DataFrame(mat, symbols)
+    CSV.write("temp.csv", df)
+    newdf = CSV.read("temp.csv")
+    rm("temp.csv")
+    return newdf
+end
