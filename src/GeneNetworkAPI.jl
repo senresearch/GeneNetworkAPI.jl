1c19f496f3c062d265bef014dc80c4587f6f2845
Saunak Sen <sen@uthsc.edu>
2022-05-25 14:22:58 -0500

    Modify export list.

--- src/GeneNetworkAPI.jl
+++ src/GeneNetworkAPI.jl
@@ -1,8 +1,19 @@
 module GeneNetworkAPI
 
-using HTTP, JSON, DataFrames, BenchmarkTools
+using HTTP, JSON, DataFrames, CSV, BenchmarkTools
 
-export check_gn
+export check_gn,
+    list_species,
+    list_groups,
+    list_datasets,
+    info_dataset,
+    info_pheno,
+    get_geno,
+    get_pheno,
+    run_gemma,
+    run_rqtl,
+    run_correlation
+
 
 include("./api_process.jl")    
 include("./query.jl")
