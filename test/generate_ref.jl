930affcdcd4ca169d7d636f2590bf7d63a8675a6
Gregory Farage <gfarage@uthsc.edu>
2022-05-27 17:25:00 -0500

    added test script files

---
+++ test/generate_ref.jl
@@ -0,0 +1,119 @@
+
+#####################################
+# Generate dataframe to be compared #
+#####################################
+
+# TEST 1 - refs
+dfSpecies = GeneNetworkAPI.DataFrame(
+            FullName = ["Mus musculus", "Rattus norvegicus"],
+            Id = [1, 2],
+            Name = ["mouse", "rat"],
+            TaxonomyId = [10090, 10116]
+);
+
+# TEST 2 - refs
+dfGroup = GeneNetworkAPI.DataFrame(
+    DisplayName = ["Hybrid Rat Diversity Panel (Includes HXB/BXH)", "UIOWA SRxSHRSP F2"],
+    FullName = ["Hybrid Rat Diversity Panel (Includes HXB/BXH)", "UIOWA SRxSHRSP F2"],
+    GeneticType = ["None", "intercross"],
+    Id = [10 ,24],
+    MappingMethodId = ["1", "1"],
+    Name = ["HXBBXH", "SRxSHRSPF2"],
+    SpeciesId = [2, 2], 
+    public = [2, 2]
+)
+
+# TEST 3 - refs
+dfGeno = GeneNetworkAPI.DataFrame(
+    Chr = ["1", "1"],
+    Locus = ["rs31443144", "rs6269442"],
+    cM = [1.5, 1.5],
+    Mb = [3.010274, 3.492195],
+    BXD1 = ["B", "B"],
+    BXD2 = ["B", "B"],
+    BXD5 = ["D", "D"] 
+)
+
+# TEST 4 - refs
+dfPheno = GeneNetworkAPI.DataFrame(
+    id = ["00077E9920", "00077E9D84"],
+    HSR_10308 = string.([223.0, 179.0]),
+    HSR_10309 = string.([226.0, 128.0]),
+    HSR_10310 = string.([123.0, 100.0]),
+    HSR_10311 = string.([70.0, 41.0]) 
+)
+
+# TEST 5 - refs
+dfInfoNonOmic = GeneNetworkAPI.DataFrame( 
+    dataset_type = ["phenotype"],
+    description = ["Central nervous system, behavior: Reaction time, premature initiations indicated by number of times that the rat pulled its snout out of the center hole before the imperative stimulus occurred and then put its snout back into the center hole without going to the left water feeder in the first 3-minute time epoch for males and females [n]"],
+    id = [10308],
+    name = ["reaction_time_pint1_5"]
+)
+
+dfInfoOmic = GeneNetworkAPI.DataFrame( 
+    confidential = [0],
+    data_scale = ["log2"],
+    dataset_type = ["mRNA expression"],
+    full_name = ["HSNIH-Palmer Nucleus Accumbens Core RNA-Seq (Aug18) rlog"],
+    id = [860],
+    name = ["HSNIH-Rat-Acbc-RSeq-0818"],
+    public = [1],
+    short_name = ["HSNIH-Palmer Nucleus Accumbens Core RNA-Seq (Aug18) rlog"],
+    tissue = ["Nucleus Accumbens mRNA"],
+    tissue_id = [17]
+)
+
+# TEST 6 - refs
+dfInfoPheno1 = GeneNetworkAPI.DataFrame( 
+    Additive = [0],
+    Id = ["log2"],
+    LRS = ["mRNA expression"],
+    locus = ["HSNIH-Palmer Nucleus Accumbens Core RNA-Seq (Aug18) rlog"],
+    PhenotypeId = [860],
+    PublicationId = ["HSNIH-Rat-Acbc-RSeq-0818"],
+    Sequence = [1]
+)
+
+dfInfoPheno2 = GeneNetworkAPI.DataFrame( 
+    additive = [2.39444435069444],
+    id = [4],
+    locus = ["rs48756159"],
+    lrs = [13.4974911471087]
+)
+
+dfInfoPheno3 = GeneNetworkAPI.DataFrame( 
+    additive = [-0.214087568058076],
+    alias  = ["HHG1; HLP3; HPE3; SMMCI; Dsh; Hhg1"],
+    chr = ["5"],
+    description = ["sonic hedgehog (hedgehog)"],
+    id = [99602],
+    locus = ["rs8253327"],
+    lrs = [12.7711275309832],
+    mb = [28.457155],
+    mean = [9.279090909090911],
+    name = ["1436869_at"],
+    p_value = [0.306],
+    se = [nothing],
+    symbol = ["Shh"],
+)
+
+# TEST 7 - refs
+
+dfGemma = GeneNetworkAPI.DataFrame( 
+    Mb = [25.100133],
+    additive = [4.513725],
+    chr = [2],
+    lod_score = [14.164224353162899],
+    name = ["rs27186439"],
+    p_value = [6.851342e-15]
+)
+
+# TEST 8 - refs
+
+dfCorrelation = GeneNetworkAPI.DataFrame( 
+    # Symbol("#_strains") = [25.100133],
+    p_value = [0.006807187408935392],
+    sample_r = [0.8928571428571429],
+    trait = ["10157"]
+)
---
+++ test/gnAPI_tests.jl
@@ -0,0 +1,82 @@
+###########################
+# TEST 1 get species list #
+###########################
+
+dfRslt1 = list_species(); 
+dfRslt2 = list_species("mouse");
+
+println("Get species list test 1: ", @test (dfRslt1[1:2,:] == dfSpecies) && 
+                                            (dfRslt2 == filter(row-> row.Name == "mouse", dfSpecies)));
+
+
+######################################
+# TEST 2 get list group of a species #
+######################################
+
+dfRslt1 = filter(row->row.Id == 10, list_groups("rat"));
+dfRslt2 = filter(row->row.Id == 24, list_groups("rat"));
+
+println("Get list group for a species test 2: ", @test ((dfRslt1 == filter(row-> row.Id == 10, dfGroup)) &&
+                                                        (dfRslt2 == filter(row-> row.Id == 24, dfGroup))));
+
+####################################
+# TEST 3 get genotypes for a group #
+####################################
+
+dfRslt = get_geno("BXD");
+dfRslt1 = filter(row->row.Locus == "rs31443144", dfRslt)[:, 1:7];
+dfRslt2 = filter(row->row.Locus == "rs6269442", dfRslt)[:, 1:7];
+
+println("Get genotypes for a group test 3: ", @test ((dfRslt1 == filter(row->row.Locus == "rs31443144", dfGeno)) &&
+                                                     (dfRslt2 == filter(row->row.Locus == "rs6269442", dfGeno))));
+
+                                                    
+######################################
+# TEST 4 get sample data for a group #
+######################################
+
+dfRslt = get_pheno("HSNIH-Palmer");
+dfRslt1 = filter(row->row.id == "00077E9920", dfRslt)[:, 1:5];
+dfRslt2 = filter(row->row.id == "00077E9D84", dfRslt)[:, 1:5];
+
+println("Get sample data for a group test 4: ", @test ((dfRslt1 == filter(row->row.id == "00077E9920", dfPheno)) &&
+                                                     (dfRslt2 == filter(row->row.id == "00077E9D84", dfPheno))));
+
+  
+#######################################
+# TEST 5 Get information about traits #
+#######################################
+
+dfRslt1 = info_dataset("HSNIH-Palmer","10308");
+dfRslt2 = info_dataset("HSNIH-Rat-Acbc-RSeq-Aug18");
+
+println("Get information about traits test 5: ", @test ((dfRslt1 == dfInfoNonOmic) && (dfRslt2 == dfInfoOmic)));
+
+
+############################################
+# TEST 6 Get summary information on traits #
+############################################
+
+dfRslt2 = info_pheno("BXD","10001");
+dfRslt3 = info_pheno("HC_M2_0606_P","1436869_at");
+
+println("Get summary information on traits test 6: ", @test ((dfRslt2 == dfInfoPheno2) && (dfRslt3 == dfInfoPheno3)));
+
+################
+# TEST 7 Gemma #
+################
+
+dfRslt = run_gemma("BXDPublish","10015",use_loco=true);
+dfRslt1 = filter(row->row.name == "rs27186439", dfRslt);
+
+println("Get Gemma results test 7: ", @test (dfRslt1 == dfGemma));
+
+######################
+# TEST 8 Correlation #
+######################
+
+dfRslt = run_correlation("1427571_at","HC_M2_0606_P","BXDPublish");
+dfRslt1 = GeneNetworkAPI.select(filter(row->row.trait == "10157", dfRslt), [2,3,4]);
+
+println("Get correlation test 8: ", @test (dfRslt1 == dfCorrelation));
+
