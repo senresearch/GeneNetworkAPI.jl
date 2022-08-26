ff630abbc19e49d46c03dbf96dc0f8db62ef9aee
Gregory Farage <gfarage@uthsc.edu>
2022-08-25 22:13:19 -0500

    made parse_geno robust to interlaced comments with # and @

--- src/parse_geno.jl
+++ src/parse_geno.jl
@@ -1,16 +1,22 @@
-# function to parse a .geno file
 
+"""                                                                                    
+    parse_geno(filename::String)
+
+Function to parse a .geno file, and returns the genotype dataframe.
+
+"""
 function parse_geno(filename::String)
     # read the file into lines
     lines = readlines(filename)
     # which lines have # as first character
     firstpound = (x->match(r"^#",x)).( lines ) .|> !isnothing
-    numpound = findfirst( diff( firstpound ) .< 0 )
-
     # which lines have @ as first character
     firstat = (x->match(r"^@",x)).( lines ) .|> !isnothing
-    numat = sum( firstat )
+    
+    # last line of comment
+    endcomment = findfirst( diff(firstat .| firstpound) .< 0 )
 
-    geno = CSV.read(filename,DataFrame,header=numpound+numat+1,delim='\t')
+    geno = CSV.read(filename, DataFrame, header=endcomment+1 , delim='\t')
+
     return geno                
 end
