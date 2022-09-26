15b111fd3e0080cd6e383cbd6ab7ede2bca011a8
GregFa <36859310+GregFa@users.noreply.github.com>
2022-09-26 00:12:03 -0500

    Update parse_geno.jl
    removed pipe to check if it was causing an error.

--- src/parse_geno.jl
+++ src/parse_geno.jl
@@ -9,9 +9,9 @@ function parse_geno(filename::String)
     # read the file into lines
     lines = readlines(filename)
     # which lines have # as first character
-    firstpound = (x->match(r"^#",x)).( lines ) .|> !isnothing
+    firstpound = (x->match(r"^#",x)).( lines ) .!= nothing
     # which lines have @ as first character
-    firstat = (x->match(r"^@",x)).( lines ) .|> !isnothing
+    firstat = (x->match(r"^@",x)).( lines ) .!= nothing
     
     # last line of comment
     endcomment = findfirst( diff(firstat .| firstpound) .< 0 )
