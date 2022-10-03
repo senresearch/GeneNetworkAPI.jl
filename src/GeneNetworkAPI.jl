d58c6037f4440f2b229d7c29afb4be95474de604
Gregory Farage <faragegr@gmail.com>
2022-10-03 11:10:51 -0500

    update

--- src/GeneNetworkAPI.jl
+++ src/GeneNetworkAPI.jl
@@ -1,24 +1,31 @@
 module GeneNetworkAPI
 
-using HTTP, JSON, DataFrames, CSV, Downloads
+    using HTTP, JSON, DataFrames, CSV, Downloads
 
-export gn_url,
-    check_gn,
-    list_species,
-    list_groups,
-    list_datasets,
-    info_dataset,
-    info_pheno,
-    get_geno,
-    get_pheno,
-    run_gemma,
-    run_rqtl,
-    run_correlation,
-    parse_geno
+    include("./api_process.jl")    
 
-include("./api_process.jl")    
-include("./query.jl")
-include("./get_data.jl")
-include("./parse_geno.jl")
+    include("./query.jl")
+    export gn_url, check_gn
+
+    include("./list_data.jl")
+    export list_species, list_groups, list_datasets, list_geno
+
+    include("./info_data.jl")
+    export info_dataset, info_pheno
+
+    include("./get_data.jl")
+    export get_geno, get_pheno
+
+    include("./run_data.jl")
+    export run_gemma, run_rqtl, run_correlation
+
+    include("./show_data.jl")
+    export show_list_geno
+
+    include("./utils_geno.jl")
+    export parse_geno, genofile_location
+
+    include("./utils.jl")
+    export make_rectangular, has_error_500
 
 end
--- src/utils_geno.jl
+++ src/utils_geno.jl
@@ -20,3 +20,57 @@ function parse_geno(filename::String)
 
     return geno                
 end
+
+"""                                                                                    
+    genofile_location(json_parsed::Dict)
+
+Returns a vector of String containing the location of geno files for a group.
+
+---
+    genofile_location(group::String)
+
+Returns a vector of String containing the location of geno files for a group.
+
+"""
+function genofile_location(json_parsed::Dict)
+    json_keys = keys(json_parsed)
+    # check if "genofile" keys exist
+    if ["genofile"] ⊈ json_keys
+        error("genofile key not found")
+    end
+
+    # check number of genofile for a group
+    n_genofile = length(json_parsed["genofile"])
+
+    vlocation_genofile = Vector{String}(undef, n_genofile)
+
+    for i in 1:n_genofile
+        ["location"] ⊆ keys(json_parsed["genofile"][i]) ?
+        vlocation_genofile[i] = json_parsed["genofile"][i]["location"] :
+        error("location key not found")
+    end
+    
+    return vlocation_genofile
+end
+
+function genofile_location(group::String; gn_url::String=gn_url())
+    # parse geno meta
+    geno_url = string(gn_url, "genotypes/", "view/", group)
+    str_json = get_api(geno_url)
+    json_parsed = parse_json(str_json)
+    
+    genofile_location(json_parsed)
+end
+
+
+"""                                                                                    
+    has_genofile_meta(group::String; gn_url::String=gn_url())
+
+Returns `true` if the group's genotype files possesse metadata that may contain 
+alternative location of the files.
+"""
+function has_genofile_meta(group::String; gn_url::String=gn_url())
+    geno_url = string(gn_url, "genotypes/", "view/", group)
+    str_json = get_api(geno_url)
+    return @inline !has_error_500(str_json)
+end
