98ed1686ad387e022d6919146cffd13817991836
GregFa <faragegr@gmail.com>
2023-10-30 17:21:02 -0500

    updated genetwork link and correlation return trait as numeric not as string

--- test/generate_ref.jl
+++ test/generate_ref.jl
@@ -139,5 +139,5 @@ dfCorrelation = GeneNetworkAPI.DataFrame(
     # Symbol("#_strains") = [25.100133],
     p_value = [0.00018929765647785504],
     sample_r = [-0.8756580854229329],
-    trait = string.([12762])
+    trait = [12762]
 )
